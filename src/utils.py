cc82013bab3016e3759152d3d9e457e052a367b2
gurneet0308 <gurneet2414@gmail.com>
2023-10-07 22:04:24 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -4,9 +4,9 @@ import sys
 import numpy as np 
 import pandas as pd
 import dill
-#import pickle
-#from sklearn.metrics import r2_score
-#from sklearn.model_selection import GridSearchCV
+import pickle
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -22,32 +22,27 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-#def evaluate_models(X_train, y_train,X_test,y_test,models,param):
-    #try:
-       # report = {}
+def evaluate_models(X_train, y_train,X_test,y_test,models):
+    try:
+        report = {}
 
-        #for i in range(len(list(models))):
-           #model = list(models.values())[i]
-            #para=param[list(models.keys())[i]]
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+        
 
-            #gs.fit(X_train,y_train)
+            model.fit(X_train, y_train)  # Train model
 
-            #model.set_params(**gs.best_params_)
-            #model.fit(X_train,y_train)
+            y_train_pred = model.predict(X_train)
 
-            ##model.fit(X_train, y_train)  # Train model
+            y_test_pred = model.predict(X_test)
 
-            #y_train_pred = model.predict(X_train)
+            train_model_score = r2_score(y_train, y_train_pred)
 
-            #y_test_pred = model.predict(X_test)
+            test_model_score = r2_score(y_test, y_test_pred)
 
-            #train_model_score = r2_score(y_train, y_train_pred)
+            report[list(models.keys())[i]] = test_model_score
 
-            #test_model_score = r2_score(y_test, y_test_pred)
-
-            #report[list(models.keys())[i]] = test_model_score
-
-        #return report
+        return report
 
     except Exception as e:
         raise CustomException(e, sys)
